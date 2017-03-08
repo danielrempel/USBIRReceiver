751baf359ff6730bec8fcfd7d4620fe2372205ed
Daniel Rempel <danil.rempel@gmail.com>
2017-03-08 18:56:23 +0200

    control.c: restyle code

--- host-side/control.c
+++ host-side/control.c
@@ -29,7 +29,8 @@ For full license text see License.txt
 
 #include "usbhelper.h"
 
-void execmd(const char* cmdWithArgs) {
+void execmd(const char* cmdWithArgs)
+{
 	FILE* proc;
 	proc = popen(cmdWithArgs,"r");
 	pclose(proc);
@@ -40,7 +41,8 @@ const uint addressPoolSize = 256;
 
 // handler for inih library
 static int iniLineHandler(void* userPtr, const char* section, const char* name,
-                   const char* value) {
+                   const char* value)
+{
 	uint sectionInt;
 	sscanf(section, "%x", &sectionInt);
 	uint nameInt;
@@ -56,46 +58,54 @@ static int iniLineHandler(void* userPtr, const char* section, const char* name,
 			return 0;
 		}
 	}
-	
+
 	// if there's already a command, remove it to put the new one
 	if(NULL != addressPool[sectionInt][nameInt]) {
 		free(addressPool[sectionInt][nameInt]);
 	}
-	
+
 	addressPool[sectionInt][nameInt] = strdup(value);
-	
+
 	return 1;
 }
 
 // Initialises arrays and reads the configuration file
-void initAddressPool() {
+void initAddressPool()
+{
 	addressPool = (char***)malloc(256 * sizeof(char**));
-	if(NULL == addressPool) {
+	if(NULL == addressPool)
+	{
 		printf("Failed to allocate address pool\n");
 		return 1;
 	}
 	int i;
-	for(i=0; i < addressPoolSize; i+=1) {
+	for(i=0; i < addressPoolSize; i+=1)
+	{
 		addressPool[i] = NULL;
 	}
 
-	if (ini_parse("usbirreceiver-config.ini", iniLineHandler, NULL) < 0) {
-        printf("Can't load 'usbirreceiver-config.ini'\n");
-        return 1;
-    }
+	if (ini_parse("usbirreceiver-config.ini", iniLineHandler, NULL) < 0)
+	{
+		printf("Can't load 'usbirreceiver-config.ini'\n");
+		return 1;
+	}
 }
 
 // Cleanup routines on exit
-int exitCleanup() {
+int exitCleanup()
+{
 	int i;
 	int j;
-	
+
 	libusb_exit(NULL);
 
-	for(i=0; i < addressPoolSize; i+=1) {
-		if(NULL != addressPool[i]) {
+	for(i=0; i < addressPoolSize; i+=1)
+	{
+		if(NULL != addressPool[i])
+		{
 			// free all the strings
-			for(j = 0; j < addressPoolSize; j+=1) {
+			for(j = 0; j < addressPoolSize; j+=1)
+			{
 				if(NULL != addressPool[i][j])
 					free(addressPool[i][j]);
 			}
@@ -104,27 +114,33 @@ int exitCleanup() {
 	}
 
 	free(addressPool);
-	
+
 	return 0;
 }
 
-void devicePoll(libusb_device_handle *deviceHandle) {
+void devicePoll(libusb_device_handle *deviceHandle)
+{
 	unsigned char bytes[8] = { 0 };
 	int len = 0;
 	int ret;
-	while (1) {
+	while (1)
+	{
 		ret = libusb_interrupt_transfer(deviceHandle,
 								1 | LIBUSB_ENDPOINT_IN,
 								bytes, 8, &len, 10000);
-		if( ret < 0 ){
+		if( ret < 0 )
+		{
 			// TODO: stay silent on timeout
 			printf("%s:%d: interrupt transfer failed: %s\n", __FILE__, __LINE__, libusb_strerror(ret));
-			if(LIBUSB_ERROR_NO_DEVICE  == ret) {
+			if(LIBUSB_ERROR_NO_DEVICE  == ret)
+			{
 				break;
 			}
 		}
-		else {			
-			if( (NULL!=addressPool[bytes[0]]) && (NULL!=addressPool[bytes[0]][bytes[2]]) ) {
+		else
+		{
+			if( (NULL!=addressPool[bytes[0]]) && (NULL!=addressPool[bytes[0]][bytes[2]]) )
+			{
 				printf("Cmd: 0x%X/0x%x -> '%s'\n",
 						bytes[0], bytes[2], addressPool[bytes[0]][bytes[2]]);
 				execmd(addressPool[bytes[0]][bytes[2]]);
@@ -136,16 +152,18 @@ void devicePoll(libusb_device_handle *deviceHandle) {
 	}
 }
 
-int main() {
+int main()
+{
 
 	initAddressPool();
-	
+
 	atexit(exitCleanup);
 
 	int i;
 	int ret = 0;
 	ret = libusb_init(NULL);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		printf("%s:%d: failed to init libusb\n", __FILE__, __LINE__);
 		return 1;
 	}
@@ -155,9 +173,11 @@ int main() {
 		// NOTE: there may be cases when the device is unreferenced by
 		// libusb_free_device_list(..) and thus made unavailable unless is
 		// opened
+		// TODO: check device ID, whether it can be used
 		libusb_device* device = findDevice(0x16C0, "danil.rempel@gmail.com", 0x05DC, "USBIRReceiver");
 
-		if( NULL == device ) {
+		if( NULL == device )
+		{
 			printf("%s:%d: Device not found!\n", __FILE__, __LINE__);
 			sleep(5);
 			continue;
@@ -166,17 +186,18 @@ int main() {
 		// connect to the device
 		libusb_device_handle *deviceHandle = NULL;
 		ret = libusb_open(device, &deviceHandle);
-		if ( LIBUSB_SUCCESS != ret ) {
+		if ( LIBUSB_SUCCESS != ret )
+		{
 			printf("%s:%d: failed to open device. Either root access or udev config required\n", __FILE__, __LINE__);
 			sleep(5);
 			continue;
 		}
-		
+
 		printf("%s:%d: Connected to the device\n", __FILE__, __LINE__);
 
 		// start polling for interrupt transfers
 		devicePoll(deviceHandle);
-		
+
 		libusb_close(deviceHandle);
 		// Maybe the device was unplugged. Wait one second before
 		// retrying
